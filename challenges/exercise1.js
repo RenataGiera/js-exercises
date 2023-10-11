f004e6a89348f08c261836f562043fde28d7cea6
Renata <renatagiera@gmail.com>
2023-10-11 18:20:18 +0100

    chore: change formating document

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,25 +1,28 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-		return sandwich.fillings;
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	return person.city === "Manchester";
+  if (person === undefined) throw new Error("person is required");
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	return Math.ceil(people / 40)
+  if (people === undefined) throw new Error("people is required");
+  return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	let newArr = arr.filter(item=>item === "sheep")
-	return newArr.length
+  if (arr === undefined) throw new Error("arr is required");
+  let newArr = arr.filter((item) => item === "sheep");
+  return newArr.length;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	return person.address.postCode[0].toUpperCase() === "M" && person.address.city === "Manchester"
+  if (person === undefined) throw new Error("person is required");
+  return (
+    person.address.postCode[0].toUpperCase() === "M" &&
+    person.address.city === "Manchester"
+  );
 }
